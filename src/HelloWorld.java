f45e2ca221f417d250921771efcd036f0299a044
Siyeon of the Ham <gkatldus1@gmail.com>
2020-11-09 13:08:11 +0900

    Code changed a little 'Hey man' part

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hey Beautiful World Hello~~~><");
+		System.out.println("Hey man This World is Beautiful Hello~");
 	}
 }
