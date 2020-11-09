7b5d1bac181027d51c17173a5f9353cd22711a5c
Siyeon of the Ham <gkatldus1@gmail.com>
2020-11-09 21:47:38 +0900

    Test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hey Beautiful World Hello~~~><");
 		System.out.println("Hey man This World is Beautiful Hello~");
+		//Test
 	}
 }
