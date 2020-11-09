c2ebcf33bcad5c64201f95c09d7ca240e73ffb34
Siyeon of the Ham <gkatldus1@gmail.com>
2020-11-09 21:54:47 +0900

    Gugudan practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,30 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hey Beautiful World Hello~~~><");
-		System.out.println("Hey man This World is Beautiful Hello~");
-		//Test
+		//2단
+		System.out.println("2단");
+		System.out.println(2 * 1);
+		System.out.println(2 * 2);
+		System.out.println(2 * 3);
+		System.out.println(2 * 4);
+		System.out.println(2 * 5);
+		System.out.println(2 * 6);
+		System.out.println(2 * 7);
+		System.out.println(2 * 8);
+		System.out.println(2 * 9);
+		
+		
+		//3단
+		System.out.println("3단");
+		System.out.println(3 * 1);
+		System.out.println(3 * 2);
+		System.out.println(3 * 3);
+		System.out.println(3 * 4);
+		System.out.println(3 * 5);
+		System.out.println(3 * 6);
+		System.out.println(3 * 7);
+		System.out.println(3 * 8);
+		System.out.println(3 * 9);
+	
 	}
 }
